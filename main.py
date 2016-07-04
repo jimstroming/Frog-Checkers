1c804d6006cacfa38254e625d72555b1335b40dc
jimstroming <ajs061@motorola.com>
2016-07-04 10:24:05 -0500

    create and android, main, kv and push files.

---
+++ main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import NumericProperty
+from kivy.clock import Clock
+import time
+#from chess import ChessEngine
+
+class RoadkillFrogRoot(BoxLayout):
+    pass
+
+
+
+class RoadKillFrogApp(App):
+    """ The kivy game app."""
+    pass
+
+
+if __name__ == '__main__':
+    RoadKillFrogApp().run()
